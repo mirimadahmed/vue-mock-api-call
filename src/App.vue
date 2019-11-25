cfedf94b8950935b98ac9adfa5f0a4e1bc7ab558
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-11-25 23:28:33 +0500

    loading ad data from api and show in block

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,49 @@
 <template>
   <div id="app">
-    <div class="ad-display" v-for="(ad, i) in adData" :key="i">
-      <div v-html="ad" />
+    <div v-if="!isLoading">
+      <div class="ad-display" v-if="adData[0] != null">
+        <div>{{ adData[0].fields.title }}</div>
+      </div>
+      <div class="ad-display" v-if="adData[1] != null">
+        <div>{{ adData[1].fields.title }}</div>
+      </div>
+      <div class="ad-display" v-if="adData[2] != null">
+        <div>{{ adData[2].fields.title }}</div>
+      </div>
+      <div class="ad-display" v-if="adData[3] != null">
+        <div>{{ adData[3].fields.title }}</div>
+      </div>
+      <div class="ad-display" v-if="adData[4] != null">
+        <div>{{ adData[4].fields.title }}</div>
+      </div>
+      <div class="ad-display" v-if="adData[5] != null">
+        <div>{{ adData[5].fields.title }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import * as service from "./api/index";
-
 export default {
   name: "app",
   data() {
     return {
       isLoading: false,
-      adData: [
-        "<p>Loading ad 1</p>",
-        "<p>Loading ad 2</p>",
-        "<p>Loading ad 3</p>"
-      ]
+      adData: [null, null, null, null, null, null]
     };
   },
   created() {
-    service.getAdData().then(adData => {
-      this.isLoading = false;
-      this.adData = adData;
-    });
+    this.isLoading = true;
+    fetch("http://dev.hellopgh.com:8000/neighborhood/brighton_heights/").then(
+      res => {
+        res.json().then(data => {
+          this.isLoading = false;
+          this.adData[0] = data.advertisements[0];
+          this.adData[1] = data.advertisements[0];
+          this.adData[2] = data.advertisements[0];
+        });
+      }
+    );
   }
 };
 </script>
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,3 @@
-let adHtml = ['<p>Loaded ad 1</p>', '<p>Loaded ad 2</p>', '<p>Loaded ad 3</p>'];
+let adHtml = ['<p>Loaded ad 1</p>', '<p>Loaded ad 2</p>', '<p>Loaded ad 3</p>', '<p>Loaded ad 4</p>', '<p>Loaded ad 5</p>', '<p>Loaded ad 6</p>'];
 
 export let getAdData = () => new Promise((resolve) => setTimeout(() => resolve(adHtml), 5000));
